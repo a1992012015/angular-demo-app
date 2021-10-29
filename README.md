be1f8103aa6c565c8d5136b537485308b04d5872
Liu Garnet <mengyehuanyu@gmail.com>
2021-10-29 14:14:15 +0800

    测试流程图

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+```mermaid
+graph TD
+  A[Cha] -->B(Bhu)
+  B --> C{Left me}
+  C -->|One| D[Lef1]
+  C -->|Tow| E[Lef2]
+  C -->|Three| F[Lef3]
+```
--- src/app/features/google-chart-test/google-chart-test.component.ts
+++ src/app/features/google-chart-test/google-chart-test.component.ts
@@ -16,32 +16,84 @@ export class GoogleChartTestComponent implements OnInit {
     type: ChartType.ColumnChart,
     rawData: [
       [
-        '2006~2010',
-        { 'v': 0.4995088279247284, 'f': '49.95%' },
-        { 'v': 0.33153241872787476, 'f': '33.15%' },
-        { 'v': 0.009430254809558392, 'f': '0.94%' },
-        { 'v': 0.02377210184931755, 'f': '2.38%' },
-        { 'v': 0.06994105875492096, 'f': '6.99%' }
+        { v: 1, f: '2006-2010' },
+        { 'v': 0.04371264949440956, 'f': '4.37%' },
+        { 'v': 0.7723124623298645, 'f': '77.23%' },
+        { 'v': 0.18397487699985504, 'f': '18.4%' }
       ],
       [
-        '2016~2020',
-        { 'v': 0.48222002387046814, 'f': '48.22%' },
-        { 'v': 0.3457760214805603, 'f': '34.58%' },
-        { 'v': 0.0024557956494390965, 'f': '0.25%' },
-        { 'v': 0.03634577617049217, 'f': '3.63%' },
-        { 'v': 0.06876228004693985, 'f': '6.88%' }
+        { v: 6, f: '2006-2010111' },
+        { 'v': 0.043, 'f': '4.3%' },
+        { 'v': 0.77, 'f': '77.2%' },
+        { 'v': 0.1839, 'f': '18%' }
+      ],
+      [
+        { v: 2, f: '2016-2020' },
+        { 'v': 0.06952574849128723, 'f': '6.95%' },
+        { 'v': 0.43073561787605286, 'f': '43.07%' },
+        { 'v': 0.4997386038303375, 'f': '49.97%' }
+      ],
+      [
+        { v: 3, f: 'No-till' },
+        { 'v': 0.009999999776482582, 'f': '1%' },
+        { 'v': 0, 'f': '0%' },
+        { 'v': 0, 'f': '0%' }
+      ],
+      [
+        { v: 4, f: 'Cover Crop' },
+        { 'v': 0.0006952575058676302, 'f': '0.07%' },
+        { 'v': 0.00430735619738698, 'f': '0.43%' },
+        { 'v': 0.004997386131435633, 'f': '0.5%' }
+      ],
+      [
+        { v: 5, f: 'Reduced Rate' },
+        { 'v': 0.0006952575058676302, 'f': '0.07%' },
+        { 'v': 0.00430735619738698, 'f': '0.43%' },
+        { 'v': 0.004997386131435633, 'f': '0.5%' }
       ]
     ],
-    columnNames: ['', 'CORN', 'SOYBEANS', 'WHEAT', 'FOREST', 'SUGARBEETS'],
+    columnNames: ['', 'NO TILL', 'CONVENTIONAL TILL', 'REDUCED TILL'],
     options: {
-      colors: ['#ffd300', '#267000', '#bf870d', '#93cc93', '#a800e2'],
       tooltip: { isHtml: true },
-      legend: {
-        position: 'top',
-        maxLines: 3,
-        alignment: 'start'
+      vAxis: { format: 'percent' },
+      hAxis: {
+        ticks: [
+          { v: 1, f: 'thirty two1' },
+          { v: 2, f: 'thirty two2' },
+          { v: 3, f: 'thirty two3' },
+          { v: 4, f: 'thirty two4' },
+          { v: 5, f: 'thirty two5' }
+        ]
       },
-      vAxis: { format: 'percent' }
+      legend: 'none',
+      colors: ['#A57C6E', '#FFF7C1', '#D47A5B']
+    }
+  };
+
+  pieChart = {
+    'type': ChartType.PieChart,
+    'rawData': [
+      ['Image unavailable or N/A', { 'v': 0.033935546875, 'f': '49.95' }],
+      ['No Emergence', { 'v': 0.52294921875, 'f': '769.79' }],
+      ['Onset Detected', { 'v': 0.02099609375, 'f': '30.91' }],
+      ['Perennial', { 'v': 0.422119140625, 'f': '621.37' }]
+    ],
+    'data': [
+      ['Image unavailable or N/A', { 'v': 0.033935546875, 'f': '49.95' }],
+      ['No Emergence', { 'v': 0.52294921875, 'f': '769.79' }],
+      ['Onset Detected', { 'v': 0.02099609375, 'f': '30.91' }],
+      ['Perennial', { 'v': 0.422119140625, 'f': '621.37' }]
+    ],
+    'columnNames': ['', ''],
+    'options': {
+      'tooltip': { 'isHtml': true },
+      'legend': 'none',
+      'pieSliceText': 'label',
+      'slices': [{ 'color': 'transparent' },
+        { 'color': '#77523A' },
+        { 'color': '#CEE14C' },
+        { 'color': '#659B2D' }
+      ]
     }
   };
 
@@ -49,6 +101,8 @@ export class GoogleChartTestComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log('localeId', this.localeId);
     console.log('barConfig', this.barConfig);
+    console.log('pieChart', this.pieChart);
   }
 }
